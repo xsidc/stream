c727cd3d3c160dcb6401823156f83038249b2f3f
Connection Refused <cutecr@pm.me>
2021-03-19 04:48:51 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
 
+	"github.com/aiocloud/stream/api"
+	"github.com/aiocloud/stream/dns"
+	"github.com/aiocloud/stream/mitm"
+)
+
+var (
+	flags struct {
+		Path string
+	}
+
+	Data struct {
+		API struct {
+			Addr   string `json:"addr"`
+			Secret string `json:"secret"`
+		} `json:"api"`
+		DNS struct {
+			Addr     string `json:"addr"`
+			MyIP     string `json:"myip"`
+			Upstream string `json:"upstream"`
+		} `json:"dns"`
+		MiTM struct {
+			HTTP []string `json:"http"`
+			TLS  []string `json:"tls"`
+		} `json:"mitm"`
+		Allowed []string `json:"allowed"`
+		Domains []string `json:"domains"`
+	}
+)
+
+func main() {
+	flag.StringVar(&flags.Path, "c", "/etc/stream.json", "Path")
+	flag.Parse()
+
+	{
+		data, err := ioutil.ReadFile(flags.Path)
+		if err != nil {
+			log.Fatalf("[APP] %v", err)
+		}
+
+		if err = json.Unmarshal(data, &Data); err != nil {
+			log.Fatalf("[APP] %v", err)
+		}
+	}
+
+	api.Secret = Data.API.Secret
+	dns.Addr = Data.DNS.Addr
+	dns.Upstream = Data.DNS.Upstream
+
+	for i := 0; i < len(Data.MiTM.HTTP); i++ {
+		mitm.ListenHTTP(Data.MiTM.HTTP[i])
+	}
+
+	for i := 0; i < len(Data.MiTM.TLS); i++ {
+		mitm.ListenTLS(Data.MiTM.TLS[i])
+	}
+
+	dns.Listen(Data.DNS.Addr, Data.Domains)
+	api.Listen(Data.API.Addr)
 }
